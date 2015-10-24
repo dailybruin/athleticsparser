96204f4a652edca3279f98e248c6b681bc460ac5
oozgur <oozgur217@gmail.com>
2015-10-24 14:05:04 -0700

    Initial commit

--- scrape.py
+++ scrape.py
@@ -1,11 +1,23 @@
 from bs4 import BeautifulSoup
 import urllib2
+import json
 
 url = "http://www.uclabruins.com//ViewContent.dbml?DB_OEM_ID=30500&CONTENT_ID=1409164"
 html = urllib2.urlopen(url).read()
 soup = BeautifulSoup(html, "html.parser")
 
-titles = soup.findAll('font', attrs={"align": "left"})
-for title in titles:
-   print title.string
+data = {}
+data["categories"] = {}
+
+names = soup.find_all('table')
+for name in names:
+   names = name.find_all('h3')
+
+   for name in range(0, len(names)):
+   		data["categories"][name] = {}
+   		data["categories"][name]["name"] = names[name].string
+
+jsonData = json.dumps(data)
+
+print jsonData
 
